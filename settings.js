4b2f6a18e92dd87aa5540966064551ad01a67d12
XyoraGanzz <100403413+XyoraGanzz@users.noreply.github.com>
2022-03-26 17:49:43 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,17 +12,17 @@ global.APIKeys = {
 
 //===> UBAH DISINI
 global.namaowner = 'Ryuuka Team' //Ganti Jadi Namamu
-global.owner = ['6285609233482','6283891921787'] //Kalo Mau Lebih Tambahin Aja
-global.nomerowner = '6285609233482' //Owner Utama Istilahnya
-global.namabot = 'RyuBotz MD' //Ganti Jadi Nama Botmu
-global.packname = '© Ryubotz MD' //Sticker Weem
-global.author = 'Ryubotz By Ryuuka Team' //Sticker Weem
+global.owner = ['62882019247701','62882019247701'] //Kalo Mau Lebih Tambahin Aja
+global.nomerowner = '62882019247701' //Owner Utama Istilahnya
+global.namabot = 'AtsairBotz MD' //Ganti Jadi Nama Botmu
+global.packname = '© Atsairbotz MD' //Sticker Weem
+global.author = 'Atsairbotz' //Sticker Weem
 global.webme = 'https://api-ryubotz.herokuapp.com'
 global.sessionName = 'session'
 global.bg_welcome = 'https://telegra.ph/file/528b594d5d91c08482515.jpg' //Max 30kb Agar Tidak Delay
 global.youtube = 'https://youtube.com/c/RyuukaBotz'
-global.github = 'https://github.com/R-TEAM94'
-global.instagram = 'https://instagram.com/fandyyy._'
+global.github = 'https://github.com/XyoraStore'
+global.instagram = 'https://instagram.com/muhammad_atsair20?utm_medium=copy_link._'
 
 //===> BACKGROUND SETIAP MENU
 global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
